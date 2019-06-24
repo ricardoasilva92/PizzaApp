bc0c805cbb540c48dad940cc964c81b168c6d752
Ricardo Silva <ricardoasilva1992@gmail.com>
2019-06-24 17:27:23 +0100

    created axios instance + replace Alert to POST request when checking out

--- src/containers/PizzaBuilder/PizzaBuilder.js
+++ src/containers/PizzaBuilder/PizzaBuilder.js
@@ -3,6 +3,7 @@ import Pizza from "../../components/Pizza/Pizza";
 import BuildControls from "../../components/Pizza/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal";
 import OrderSummary from "../../components/Pizza/OrderSummary/OrderSummary";
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
 	sauce: 1,
@@ -53,7 +54,23 @@ class PizzaBuilder extends Component {
 	}
 
 	purshaseContinueHandler = () => {
-		alert("Pizza Time");
+		//needs to have.json because of firebase
+		const order ={
+			ingredients: this.state.ingredients,
+			price: this.state.totalPrice, //price should be calculated in server
+			customer: {
+				name: 'Ricardo',
+				address: {
+					street: 'testStreet',
+					zipCode: '41351',
+					country: 'Portugal'
+				},
+				email:'ric@MediaList.com'
+			}
+		}
+		axios.post('/orders.json',order)
+			.then(response => console.log(response))
+			.catch(error => console.log(error));
 	};
 
 	purshaseCancelOrder = () => {
