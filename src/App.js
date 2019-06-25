2a16da719a8506bdf5c453b9a39f37f04610b6dc
Ricardo Silva <ricardoasilva1992@gmail.com>
2019-06-25 20:52:32 +0100

    Add routing to app.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
+
 import Layout from "./components/Layout/Layout";
-import Pizza from "./containers/PizzaBuilder/PizzaBuilder";
-import Checkout from './containers/Checkout/Checkout'
+import Checkout from "./containers/Checkout/Checkout";
+import PizzaBuilder from "./containers/PizzaBuilder/PizzaBuilder";
 
 function App() {
 	return (
 		<div>
 			<Layout>
-				<Pizza />
-				<Checkout/>
+				<Switch>
+					<Route path="/checkout" component={Checkout} />
+					<Route path="/" exact component={PizzaBuilder} />
+				</Switch>
 			</Layout>
 		</div>
 	);
